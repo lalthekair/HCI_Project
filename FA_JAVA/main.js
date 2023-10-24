c3b8be71e4ae617045075c877d4f0866800a0306
Falamoudi15 <118709096+Falamoudi15@users.noreply.github.com>
2023-10-24 22:52:41 +0300

    Add files via upload

---
+++ FA_JAVA/main.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text", {
+    strings:["AI Specilist", "Data Scientist", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
